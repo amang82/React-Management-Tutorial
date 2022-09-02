8712a8e9504b104e9ecfa3b8602df5abc7171d38
genius82kr <genius82kr@gmail.com>
2022-09-02 14:53:49 +0900

    Create Customer.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,49 @@
-import logo from "./logo.svg";
 import "./App.css";
+import Customer from "./components/Customer";
+
+const customers = [
+  {
+    id: 1,
+    Image: "http://placeimg.com/64/64/1",
+    name: "kim",
+    birthday: "900102",
+    gender: "M",
+    job: "job",
+  },
+  {
+    id: 2,
+    Image: "http://placeimg.com/64/64/2",
+    name: "choi",
+    birthday: "900502",
+    gender: "M",
+    job: "job",
+  },
+  {
+    id: 3,
+    Image: "http://placeimg.com/64/64/3",
+    name: "park",
+    birthday: "900103",
+    gender: "M",
+    job: "job",
+  },
+];
 
 function App() {
   return (
-    <div className="App">
-      <img src={logo} className="App-logo" alt="logo" />
-      <h2>Test</h2>
+    <div>
+      {customers.map((c) => {
+        return (
+          <Customer
+            key={c.id}
+            id={c.id}
+            Image={c.Image}
+            name={c.name}
+            birthday={c.birthday}
+            gender={c.gender}
+            job={c.job}
+          />
+        );
+      })}
     </div>
   );
 }
